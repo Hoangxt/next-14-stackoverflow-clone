0b2c370308d9782b5dc2146e94ab1acc121d64be
Dinh Huy Hoang <80768175+Hoangxt@users.noreply.github.com>
2023-12-19 23:22:07 +0700

    modi home page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,52 @@
+import QuestionCard from "@/components/cards/QuestionCard";
 import HomeFilters from "@/components/home/HomeFilters";
 import Filter from "@/components/shared/Filter/Filter";
+import NoResult from "@/components/shared/NoResult/NoResult";
 import LocalSearchBar from "@/components/shared/search/LocalSearchBar";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
 import Link from "next/link";
 import React from "react";
+
+const questions = [
+  {
+    _id: "1",
+    title: "Cascading Deletes in SQLAlchemy",
+    tags: [
+      { _id: "1", name: "python" },
+      { _id: "2", name: "sql" },
+    ],
+    author: {
+      _id: "1",
+      clerkId: "123",
+      name: "John Doe",
+      picture: "url_to_picture",
+    },
+    upvotes: 76756464,
+    views: 3453453534,
+    answers: [],
+    createdAt: new Date("2023-09-01T12:00:00.000Z"),
+  },
+  {
+    _id: "2",
+    title: "How to center a div?",
+    tags: [
+      { _id: "3", name: "css" },
+      { _id: "4", name: "scss" },
+    ],
+    author: {
+      _id: "2",
+      clerkId: "456",
+      name: "Jane Doe",
+      picture: "url_to_picture",
+    },
+    upvotes: 9,
+    views: 120,
+    answers: [],
+    createdAt: new Date("2023-09-02T14:30:00.000Z"),
+  },
+  // Add more questions as needed
+];
 
 const Home = () => {
   return (
@@ -35,6 +77,33 @@ const Home = () => {
       </div>
 
       <HomeFilters />
+
+      {/*  */}
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {/* Loop through questions */}
+        {questions.length > 0 ? (
+          questions.map((question) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+        ) : (
+          <NoResult
+            title="There is no question to show"
+            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. our query could be the next big thing others learn from. Get involved! 💡"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
+      </div>
     </>
   );
 };
